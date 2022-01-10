5344c0edd1e5fc8a65b109792b43cba9c252ec6f
seongjin_song <seongjin_song@tmax.co.kr>
2022-01-11 03:01:34 +0900

    feat : 주문 검색 기능 개발

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -16,5 +17,22 @@ public class OrderRepository {
     public Order findOne(Long id){
         return em.find(Order.class, id);
     }
-//    public List<Order> findAll(OrderSearch orderSearch){}
+    public List<Order> findAll(OrderSearch orderSearch){
+        return em.createQuery("select o from Order o join o.member m where o.status = :status and m.name like :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                .setFirstResult(100)
+                .setMaxResults(1000) //최대 1000건
+                .getResultList();
+    }
+    /*
+        myBatis는 동적 쿼리생성하는게 편하다는 장점이 있다.
+     */
+    /**
+     * JPA Criteria -- JPA 표준 스펙으로 제공한다.
+     * 하지만 권장하는 방법이 아니다. -> 실무에서 안쓴다.
+     */
+    /**
+     * 실무에서는 QueryDSL을 사용한다.
+     */
 }
