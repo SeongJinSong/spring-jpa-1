41f87dc5626409f3cce56527c39477e2af869e3c
seongjin_song <seongjin_song@tmax.co.kr>
2022-01-22 12:55:24 +0900

    주문 조회 V2 : 엔티티를 DTO로 변환

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -2,13 +2,18 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,4 +31,50 @@ public class OrderApiController {
         }
         return all;
     }
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2(){
+        return orderRepository.findAllByString(new OrderSearch()).stream()
+                .map(OrderDto::new)
+                .collect(Collectors.toList());
+    }
+
+    //@Data //toString 등 여러가지를 제공한다.
+    @Getter
+    static class OrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+//        private List<OrderItem> orderItems;
+        private List<OrderItemDto> orderItems;
+
+        /**
+         * DTO안에 엔티티가 있으면 안된다!!
+         * OrderItem 조차도 Dto로 변경해야한다! > 이걸 캐치하지 못하는 경우가 실제로 많다.
+         */
+
+        public OrderDto(Order order){
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+//            order.getOrderItems().stream().forEach(o->o.getItem().getName()); // 초기화 필요
+//            orderItems = order.getOrderItems();// 위 초기화 없으면 null로 나온다.
+            orderItems = order.getOrderItems().stream().map(OrderItemDto::new).collect(Collectors.toList())
+        }
+    }
+
+    @Getter
+    static class OrderItemDto{
+        private String itemName; //상품 명
+        private int orderPrice; //주문 가격
+        private int count; //주문 수량
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
+    }
 }
+
